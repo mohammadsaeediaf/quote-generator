1c6aa6524cf41f70ea37d3cd12ee4c08b97f5997
VanGuard <alisaadati18000@gmail.com>
2023-10-23 23:19:35 +0330

    Fetch quotes with external API

--- script.js
+++ script.js
@@ -3,20 +3,9 @@ const button = document.querySelector(".generate-btn");
 button.addEventListener("click", addquotes);
 
 function addquotes() {
-  const quotes = {
-    "-Steve Jobs":
-      "I believe life is an intelligent thing: that things aren't random.",
-    "-Mihaly Csikszentmihalyi": "Goals transform a random walk into a chase.",
-    "-Albert Camus":
-      "It is necessary to fall in love... if only to provide an alibi for all the random despair you are going to feel anyway.",
-    "-Sam Harris":
-      "Human well-being is not a random phenomenon. It depends on many factors - ranging from genetics and neurobiology to sociology and economics. But, clearly, there are scientific truths to be known about how we can flourish in this world. Wherever we can have an impact on the well-being of others, questions of morality apply.",
-  };
-
-  const authors = Object.keys(quotes);
-  const length = authors[Math.floor(Math.random() * authors.length)];
-  const quote = quotes[length];
-
-  document.querySelector(".quote").innerHTML = quote;
-  document.querySelector(".author").innerHTML = length;
+  fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {});
 }

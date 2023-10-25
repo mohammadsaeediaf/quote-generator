b442fa33fd6212b0629447cb2af151390ba0e96c
VanGuard <alisaadati18000@gmail.com>
2023-10-25 23:17:07 +0330

    add links section

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ const button = document.querySelector(".generate-btn");
 button.addEventListener("click", addquotes);
 
 function addquotes() {
-  fetch("https://type.fit/api/quotes")
+  fetch("https://www.stands4.com/services/v2/quotes.php")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data[Math.floor(Math.random(0) * 6)].text);
-      console.log();
-      const text = document.querySelector(".quote");
-      text.innerHTML = data[Math.floor(Math.random(0) * 6)].text;
-      const author = document.querySelector(".author");
-      author.innerHTML = data[Math.floor(Math.random(0) * 6)].author.replace(
-        ", type.fit",
-        " "
-      );
+      // console.log(data[Math.floor(Math.random(0) * 6)].text);
+      // console.log();
+      // const text = document.querySelector(".quote");
+      // text.innerHTML = data[Math.floor(Math.random(0) * 6)].text;
+      // const author = document.querySelector(".author");
+      // author.innerHTML = data[Math.floor(Math.random(0) * 6)].author.replace(
+      //   ", type.fit",
+      //   " "
+      // );
+
+      console.log(data);
+
+      data.forEach((element) => {
+        console.log(element.author);
+      });
     });
 }

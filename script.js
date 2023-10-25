5563b591374f53127fc7a12a7dec6c2c52d790eb
VanGuard <alisaadati18000@gmail.com>
2023-10-25 23:21:26 +0330

    add sound functtion

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 const button = document.querySelector(".generate-btn");
 
 button.addEventListener("click", addquotes);
+soundBtn.addEventListener("click", sound);
 
 function addquotes() {
-  fetch("https://www.stands4.com/services/v2/quotes.php")
+  btn.innerHTML = "Loading Quote...";
+  fetch("https://type.fit/api/quotes")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // console.log(data[Math.floor(Math.random(0) * 6)].text);
-      // console.log();
-      // const text = document.querySelector(".quote");
-      // text.innerHTML = data[Math.floor(Math.random(0) * 6)].text;
-      // const author = document.querySelector(".author");
-      // author.innerHTML = data[Math.floor(Math.random(0) * 6)].author.replace(
-      //   ", type.fit",
-      //   " "
-      // );
-
-      console.log(data);
-
-      data.forEach((element) => {
-        console.log(element.author);
-      });
+      let counter = Math.floor(Math.random(1) * 16);
+      const text = data[counter].text;
+      const author = data[counter].author;
+      document.querySelector(".quote").innerHTML = text;
+      document.querySelector(".author").innerHTML = author;
+      btn.innerHTML = "New Quote";
     });
+}
+function sound() {
+  let soundOn = new SpeechSynthesisUtterance(
+    `${quoteText.innerHTML} by ${author.innerHTML}`
+  );
+  speechSynthesis.speak(soundOn);
 }
